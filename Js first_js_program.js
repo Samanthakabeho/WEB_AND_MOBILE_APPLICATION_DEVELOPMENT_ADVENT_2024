3e10b84a0c6b457f9f4f17f62147476fd1abfb35
Samanthakabeho <samanthakabeho@gmail.com>
2024-09-16 22:48:39 +0300

    Add files via upload

---
+++ Js first_js_program.js
@@ -0,0 +1,12 @@
+// This is my first JS application
+
+console.log("Hello world")
+console.log("Kabeho Samantha")
+console.log("B27433")
+console.log("BSIT")
+
+
+
+l
+
+
---
+++ WEEK 2/Variables.js
@@ -0,0 +1,30 @@
+// Innitialization
+var my_name = "Samantha";
+
+//Declare a variable
+var height;
+
+// Assign a variable
+height = 3.4;
+console.log(height);
+
+// let
+let area = 90.5;
+console.log("Area: ",area);
+
+let f_name;
+
+f_name = "Samantha";
+console.log(" First Name: ",f_name);
+
+//const
+// Innitialize
+const pie = 3.14;
+
+// Declare
+//const k;
+
+// k= 34;
+
+
+
